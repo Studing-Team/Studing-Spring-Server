eeabbd50970fa21c7d50734aceb7ce219f4afd07
softmoca <acyc0909@gmail.com>
2024-10-15 11:33:18 +0900

    feat : 로그인 필터 pricessesURL 변경

--- src/main/java/studing/studing_server/auth/jwt/JWTFilter.java
+++ src/main/java/studing/studing_server/auth/jwt/JWTFilter.java
@@ -26,60 +26,38 @@ public class JWTFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        //request에서 Authorization 헤더를 찾음
         String authorization= request.getHeader("Authorization");
-        System.out.println("dddd");
-        System.out.println("dddd");
-        System.out.println("dddd");
         System.out.println(authorization);
-        System.out.println("dddd");
-        System.out.println("dddd");
-        System.out.println("dddd");
-
-
-
-
 
         //Authorization 헤더 검증
         if (authorization == null || !authorization.startsWith("Bearer ")) {
-
             System.out.println("token null");
             filterChain.doFilter(request, response);
-
-            //조건이 해당되면 메소드 종료 (필수)
             return;
         }
 
         System.out.println("authorization now");
-        //Bearer 부분 제거 후 순수 토큰만 획득
         String token = authorization.split(" ")[1];
 
-        //토큰 소멸 시간 검증
         if (jwtUtil.isExpired(token)) {
-
             System.out.println("token expired");
             filterChain.doFilter(request, response);
-
-            //조건이 해당되면 메소드 종료 (필수)
             return;
         }
 
-        //토큰에서 username과 role 획득
         String username = jwtUtil.getLoginIdentifier(token);
         String role = jwtUtil.getRole(token);
 
-        //userEntity를 생성하여 값 set
         Member member = new Member();
         member.setLoginIdentifier(username);
         member.setPassword("temppassword");
         member.setRole(role);
 
-        //UserDetails에 회원 정보 객체 담기
         CustomMemberDetails customMemberDetails = new CustomMemberDetails(member);
 
-        //스프링 시큐리티 인증 토큰 생성
+
         Authentication authToken = new UsernamePasswordAuthenticationToken(customMemberDetails, null, customMemberDetails.getAuthorities());
-        //세션에 사용자 등록
+
         SecurityContextHolder.getContext().setAuthentication(authToken);
 
         filterChain.doFilter(request, response);
