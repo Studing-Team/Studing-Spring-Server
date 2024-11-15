0746c5a3079fd602e0f0bd9d70119df42f3e3df1
softmoca <acyc0909@gmail.com>
2024-11-15 13:12:43 +0900

    feat : FCM 로깅 추가

--- src/main/java/studing/studing_server/config/FCMConfig.java
+++ src/main/java/studing/studing_server/config/FCMConfig.java
@@ -5,29 +5,36 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 
+@Slf4j
 @Configuration
 public class FCMConfig {
-
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
-        // 파일 경로 수정
-        GoogleCredentials googleCredentials = GoogleCredentials
-                .fromStream(new ClassPathResource("studing-fcm-firebase-adminsdk-ltwke-d4d183baf8.json").getInputStream());
+        log.info("Initializing Firebase Messaging...");
+        try {
+            GoogleCredentials googleCredentials = GoogleCredentials
+                    .fromStream(new ClassPathResource("studing-fcm-firebase-adminsdk-ltwke-d4d183baf8.json").getInputStream());
 
-        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
-                .setCredentials(googleCredentials)
-                .build();
+            FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                    .setCredentials(googleCredentials)
+                    .build();
 
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(firebaseOptions);
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(firebaseOptions);
+                log.info("Firebase Application has been initialized");
+            }
+
+            return FirebaseMessaging.getInstance();
+        } catch (Exception e) {
+            log.error("Failed to initialize Firebase: ", e);
+            throw e;
         }
-
-        return FirebaseMessaging.getInstance();
     }
 }
