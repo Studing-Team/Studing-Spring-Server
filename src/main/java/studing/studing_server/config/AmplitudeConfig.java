3bee3b7484b0c60f08fbcd5fc8bc7f98b69bb761
softmoca <acyc0909@gmail.com>
2024-11-29 17:50:22 +0900

    deply : 엠플 키

--- src/main/java/studing/studing_server/config/AmplitudeConfig.java
+++ src/main/java/studing/studing_server/config/AmplitudeConfig.java
@@ -2,15 +2,20 @@ package studing.studing_server.config;
 
 import com.amplitude.Amplitude;
 
+import java.util.Properties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class AmplitudeConfig {
 
-    @Value("${amplitude.api-key}")
-    private String apiKey;
+    private final String apiKey;
+
+    public AmplitudeConfig(@Value("${amplitude.api-key}") String apiKey) {
+        this.apiKey = apiKey;
+    }
 
     @Bean
     public Amplitude amplitudeClient() {
